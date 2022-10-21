fe85d952b9828784245ddb642b46eeeba833c099
madz42 <fv.sqn.mad@gmail.com>
2022-10-21 15:04:24 +0200

    bcrypt del

--- seeders/01-some-users.js
+++ seeders/01-some-users.js
@@ -1,5 +1,5 @@
 "use strict";
-const bcrypt = require("bcrypt")
+// const bcrypt = require("bcrypt")
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -10,26 +10,26 @@ module.exports = {
           name: "Apple",
           email: "apple@apple.com",
           phone: 1234567,
-          password: bcrypt.hashSync("apple", 10),
+          // password: bcrypt.hashSync("apple", 10),
           createdAt: new Date(),
           updatedAt: new Date(),
-        }, 
+        },
         {
           name: "Banana",
           email: "banana@banana.com",
           phone: 1234567,
-          password: bcrypt.hashSync("banana", 10),
+          // password: bcrypt.hashSync("banana", 10),
           createdAt: new Date(),
           updatedAt: new Date(),
-        },  
+        },
         {
           name: "Coco",
           email: "coco@coco.com",
           phone: 1234567,
-          password: bcrypt.hashSync("coco", 10),
+          // password: bcrypt.hashSync("coco", 10),
           createdAt: new Date(),
           updatedAt: new Date(),
-        },     
+        },
       ],
       {}
     );
